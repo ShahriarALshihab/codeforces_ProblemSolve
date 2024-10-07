7c2a1cbffa83a6140a540a06905f560cb0b452e7
shihab <shahriarshihab96@gmail.com>
2024-10-08 05:42:16 +0600

    first commit

---
+++ domino.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cout<<"enter number:"; 
+    cin>> m >> n;
+
+    int multi=m*n;
+    int output=multi/2;
+    cout<<output;
+
+return 0;
+}
